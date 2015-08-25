b33cf8ce8482891bc0623a04441c0238783d011c
dwhelan <dwhelan@dpwhelan.com>
2015-08-25 14:42:06 -0400

    Added coveralls.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,20 @@
 require 'rspec'
 # require 'rspec/its'
-# require 'coveralls'
+require 'coveralls'
 
 require 'simplecov'
 
-# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-#     SimpleCov::Formatter::HTMLFormatter,
-#     Coveralls::SimpleCov::Formatter
-# ]
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+]
 SimpleCov.start
 
 # require 'pry'
 # require 'awesome_print'
 
 # I18n.enforce_available_locales = true
-# Coveralls.wear!
+Coveralls.wear!
 
 require 'delegate_matcher'
 
