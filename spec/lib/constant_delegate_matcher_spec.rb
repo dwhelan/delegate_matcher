4bbe123d4fc1a5f1d63d0ac6ff9ca8575b8f2c44
dwhelan <dwhelan@dpwhelan.com>
2015-12-03 20:53:25 -0500

    Fixed rubocop errors

--- spec/lib/constant_delegate_matcher_spec.rb
+++ spec/lib/constant_delegate_matcher_spec.rb
@@ -6,9 +6,7 @@ module RSpec
   module Matchers
     module DelegateMatcher
       module ToConstant
-
         describe 'DelegateTo matcher with constants' do
-
           class Post
             AUTHOR = Object.new
           end
@@ -16,53 +14,53 @@ module RSpec
           subject { Post.new }
 
           it_behaves_like 'a simple delegator', :name, :AUTHOR do
-            before {
+            before do
               class Post
                 def name
                   AUTHOR.name
                 end
               end
-            }
+            end
           end
 
           it_behaves_like 'a delegator with a nil check', :name, :AUTHOR do
-            before {
+            before do
               class Post
                 def name
                   AUTHOR.name if AUTHOR
                 end
               end
-            }
+            end
           end
 
           it_behaves_like 'a delegator with args', :name, :AUTHOR, :arg1 do
-            before {
+            before do
               class Post
                 def name(*args)
                   AUTHOR.name(*args)
                 end
               end
-            }
+            end
           end
 
           it_behaves_like 'a delegator with a block', :name, :AUTHOR do
-            before {
+            before do
               class Post
                 def name(&block)
                   AUTHOR.name(&block)
                 end
               end
-            }
+            end
           end
 
           it_behaves_like 'a delegator with args and a block', :name, :AUTHOR, :arg1 do
-            before {
+            before do
               class Post
                 def name(*args, &block)
                   AUTHOR.name(*args, &block)
                 end
               end
-            }
+            end
           end
         end
       end
