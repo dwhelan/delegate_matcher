91f8f4aa80bf15b4d1fe7449eece28821851f5fa
dwhelan <dwhelan@dpwhelan.com>
2015-12-04 16:28:03 -0500

    Added ability to stub class methods to Stubber

--- lib/delegate_matcher/delegate_to_class_variable.rb
+++ lib/delegate_matcher/delegate_to_class_variable.rb
@@ -2,12 +2,8 @@ module RSpec
   module Matchers
     module DelegateMatcher
       class DelegateToClassVariable < DelegateTo
-        def do_delegate(test_delegate = delegate_double)
-          actual_delegate = delegator.sender.class.class_variable_get(delegate)
-          delegator.sender.class.class_variable_set(delegate, test_delegate)
-          yield
-        ensure
-          delegator.sender.class.class_variable_set(delegate, actual_delegate)
+        def do_delegate(test_delegate = delegate_double, &block)
+          Stubber.stub(delegator.sender, delegate, test_delegate, &block)
         end
       end
     end
--- lib/delegate_matcher/stubber.rb
+++ lib/delegate_matcher/stubber.rb
@@ -1,16 +1,47 @@
 module RSpec
   module Matchers
     module DelegateMatcher
-      module Stubber
-        def stub(sender, name, value, &block)
-          original_value = sender.instance_variable_get(name)
-          sender.instance_variable_set(name, value)
-          block.call
-        ensure
-          sender.instance_variable_set(name, original_value)
-        end
+      class Stubber
+        class << self
+          def stub(sender, name, value, &block)
+            case
+            when is_a_class_variable?(name)
+              stub_class_variable(sender, name, value, &block)
+            when is_an_instance_variable?(name)
+              stub_instance_variable(sender, name, value, &block)
+            else
+              raise 'WTF'
+            end
+          end
 
-        module_function :stub
+          private
+
+          def is_a_class_variable?(name)
+            name.to_s.start_with?('@@')
+          end
+
+          def is_an_instance_variable?(name)
+            name.to_s.start_with?('@')
+          end
+
+
+          def stub_class_variable(sender, name, value, &block)
+            klass = sender.class
+            original_value = klass.class_variable_get(name)
+            klass.class_variable_set(name, value)
+            block.call
+          ensure
+            klass.class_variable_set(name, original_value)
+          end
+
+          def stub_instance_variable(sender, name, value, &block)
+            original_value = sender.instance_variable_get(name)
+            sender.instance_variable_set(name, value)
+            block.call
+          ensure
+            sender.instance_variable_set(name, original_value)
+          end
+        end
       end
     end
   end
