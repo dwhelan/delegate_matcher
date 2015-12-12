265080e536baa3936f6ef8633ae2f657f6927e42
dwhelan <dwhelan@dpwhelan.com>
2015-12-12 16:45:53 -0500

    Got without a nil check working

--- spec/lib/shared/a_delegator_without_a_nil_check.rb
+++ spec/lib/shared/a_delegator_without_a_nil_check.rb
@@ -1,5 +1,5 @@
 shared_examples 'a delegator without a nil check' do
-  # it { should     delegate(:name).to(receiver).allow_nil(false)  }
-  # it { should_not delegate(:name).to(receiver).allow_nil(true)  }
-  # it { should_not delegate(:name).to(receiver).allow_nil  }
+  it { should     delegate(:name).to(receiver).allow_nil(false)  }
+  it { should_not delegate(:name).to(receiver).allow_nil(true)  }
+  it { should_not delegate(:name).to(receiver).allow_nil  }
 end
